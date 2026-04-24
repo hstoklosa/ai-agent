939ff6d97a03ece7ded4a02cffc5b5f7e929869f
hstoklosa <hubert.stoklosa23@gmail.com>
2026-04-24 20:17:25 +0100

    feat: implement llm function for writing to files

---
+++ test_write_file.py
@@ -0,0 +1,17 @@
+from functions.write_file import write_file
+
+def test():
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print("Result for 'calculator/lorem.txt' file:")
+    print(result)
+
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print("Result for 'calculator/pkg/morelorem.txt' file:")
+    print(result)
+
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print("Result for 'calculator/tmp/temp.txt' file:")
+    print(result)
+
+if __name__ == "__main__":
+    test()
